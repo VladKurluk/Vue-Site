2da7e2171eb78592e8bcb62742c3c0df431de540
VladKurluk <vlad11kurluk89@gmail.com>
2018-07-06 15:34:02 +0300

    Описал компонент Главной страницы и добавил глобальный ф-л стилей

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
-  <h1>Home</h1>
+  <div class="home main-content">
+    <div class="lef-content">
+      <h1>20 YRS DESIGN</h1>
+
+      <p class="subtxt">Gary has been a full stack developer for 2 decades and an instructor for the last 10 years.</p>
+      <p class="subtxt">I've worked with LinkedIn, Lynda, Pluralsight and more.</p>
+    </div>
+
+    <div class="right-content img1">
+      <button><router-link to="/portfolio/one" />Browse Work</button>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
+  name: 'home',
+  comments: {
 
+  }
 }
 </script>
 
 <style scoped>
 
-  
+  .img1 {
+    background: url('../assets/home.jpg');
+  }
 
 </style>
