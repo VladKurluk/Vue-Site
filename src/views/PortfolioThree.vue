5097f108813beaadca55357467ca22d00f3cb3e1
VladKurluk <vlad11kurluk89@gmail.com>
2018-07-09 17:59:24 +0300

    Начал разрабатывать компоненты 2-й и 3-й страницы портфолио

---
+++ src/views/PortfolioThree.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home main-content">
+    <div class="lef-content slideUp">
+      <h1>Crypto Logo</h1>
+
+      <p class="subtxt">Adipisicing labore dolore proident non nostrud sint cupidatat labore.</p>
+      <p class="subtxt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur culpa itaque ipsa sapiente rem delectus in. Sequi inventore, numquam aut veniam totam, ipsa cumque quisquam sit itaque magni modi vitae.</p>
+
+      <router-link to="/portfolio/three"><img src="../assets/arrow-down.svg" class="arrow arrow-down" alt="Arrow down Link"></router-link>
+    </div>
+
+    <div class="right-content img2">
+      <img src="../assets/logo.svg" alt="Logo">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'one'
+}
+</script>
+
+<style lang="scss" scoped>
+
+  $page_color: #625B7E;
+
+  .lef-content {
+
+    h1 {
+      text-transform: uppercase;
+      color: $page_color;
+    }
+  }
+
+  .img2 {
+    background: $page_color;
+
+    img {
+      width: 50%;
+    }
+  }
+
+</style>
---
+++ src/views/PortfolioTwo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="three main-content">
+    <div class="lef-content slideUp">
+
+        <router-link to="/portfolio/two"><img src="../assets/arrow-up.svg" class="arrow arrow-up" alt="Arrow up Link"></router-link>
+
+        <h1>Print</h1>
+
+        <p class="subtxt">Adipisicing labore dolore proident non nostrud sint cupidatat labore.</p>
+        <p class="subtxt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur culpa itaque ipsa sapiente rem delectus in. Sequi inventore, numquam aut veniam totam, ipsa cumque quisquam sit itaque magni modi vitae.</p>
+
+        <router-link to="/portfolio/three"><img src="../assets/arrow-down.svg" class="arrow arrow-down" alt="Arrow down Link"></router-link>
+    </div>
+
+    <div class="right-content img2">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'three'
+}
+</script>
+
+<style lang="scss" scoped>
+
+  $page_color: #48b886;
+
+  .lef-content {
+
+    h1 {
+      text-transform: uppercase;
+      color: $page_color;
+    }
+  }
+
+  .img2 {
+    background: url('../assets/phone.jpg');
+    background-size: cover;
+    background-position: 50%;
+  }
+
+
+</style>
