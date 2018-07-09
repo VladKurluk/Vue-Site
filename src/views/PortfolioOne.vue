48bbe35f16683c0008dd4d0fc8110aea0f46fc34
VladKurluk <vlad11kurluk89@gmail.com>
2018-07-09 14:45:03 +0300

    Создал компонент для 1-й страницы портфолио

---
+++ src/views/PortfolioOne.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home main-content">
+    <div class="lef-content">
+      <h1>Crypto Logo</h1>
+
+      <p class="subtxt">Adipisicing labore dolore proident non nostrud sint cupidatat labore.</p>
+      <p class="subtxt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur culpa itaque ipsa sapiente rem delectus in. Sequi inventore, numquam aut veniam totam, ipsa cumque quisquam sit itaque magni modi vitae.</p>
+
+      <router-link to="/portfolio/two"><img src="../assets/arrow-down.svg" class="arrow arrow-down" alt="Arrow down Link"></router-link>
+    </div>
+
+    <div class="right-content img2">
+      <img src="../assets/logo.svg" alt="Logo">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'one'
+}
+</script>
+
+<style scoped>
+
+  .img1 {
+    background: url('../assets/home.jpg');
+    background-size: cover;
+  }
+
+</style>
