9ce650ea6417aa744e24843b999fb502bba039c0
VladKurluk <vlad11kurluk89@gmail.com>
2018-07-10 11:07:41 +0300

    Сделал компонент страницы About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>About</h1>
+  <div class="about main-content">
+    <div class="left-content">
+      <h1>About me</h1>
+      <p class="subtxt">At the moment, for me in the priority development of Web applications using framework Vue.js. My goal is to get work on interesting projects that will benefit the human being. For me it is important to be constantly in the process of improvement and development.</p>
+
+      <router-link class="button-link" to="/portfolio/one">Look at my work</router-link>
+    </div>
+    <div class="right-content">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet deserunt illum laborum, consequatur delectus, facilis, veritatis quo doloribus earum eveniet explicabo sed. Reprehenderit, dolorum! Perspiciatis dignissimos sit ratione libero reiciendis.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet deserunt illum laborum, consequatur delectus, facilis, veritatis quo doloribus earum eveniet explicabo sed. Reprehenderit, dolorum! Perspiciatis dignissimos sit ratione libero reiciendis.</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,8 +20,27 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+  .left-content {
+    background-color: #ffe476;
+  }
  
+  .right-content {
+    background-color: #fff;
+    padding: 50px;
+    margin-top: 80px;
+    display: block;
+  }
+
+  .button-link {
+    font-size: 1.5em;
+  }
+
+  .subtxt {
+    font-size: 1.5em;
+    margin-top: 30px;
+    margin-bottom: 100px;
+  }
 
 </style>
